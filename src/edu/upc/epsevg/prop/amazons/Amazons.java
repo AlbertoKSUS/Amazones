0e77464aed4c4ffdb43312ce3fbd497cabb15246
alpez <alpez@DESKTOP-49NFB9F>
2020-12-17 17:17:52 +0100

    solucionar pequeño error al finalizar si perdemos, gana al profesor

--- src/edu/upc/epsevg/prop/amazons/Amazons.java
+++ src/edu/upc/epsevg/prop/amazons/Amazons.java
@@ -4,6 +4,7 @@ import edu.upc.epsevg.prop.amazons.players.HumanPlayer;
 import edu.upc.epsevg.prop.amazons.players.CarlinhosPlayer;
 import edu.upc.epsevg.prop.amazons.players.RandomPlayer;
 import edu.upc.epsevg.prop.amazons.players.NouGroup;
+import edu.upc.epsevg.prop.amazons.players.amazonxes;
 import javax.swing.SwingUtilities;
 
 /**
@@ -21,8 +22,8 @@ public class Amazons {
             @Override
             public void run() {
                 
-                IPlayer player1 = new RandomPlayer("pacp");
-                IPlayer player2 = new NouGroup("hol");
+                IPlayer player1 = new amazonxes("hola", 2);
+                IPlayer player2 = new CarlinhosPlayer();
                 
                 new AmazonsBoard(player1 , player2, 10, Level.QUARTERBOARD);
                 
